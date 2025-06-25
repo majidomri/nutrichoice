b85b713904b5a1c742baf5d5ba0983c2a6053d5b
Majid Omri <majidomri.hse@outlook.com>
2025-06-25 12:16:12 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./nutri.html", "./main.js", "./**/*.html", "./**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
